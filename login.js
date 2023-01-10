098663f614f5ead55647366c16f8cd7026494736
Hedy <duej123456@gmail.com>
2023-01-10 18:32:24 +0800

    debug

--- login.js
+++ login.js
@@ -1,7 +1,5 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 
-const baseUrl = "https://vue3-course-api.hexschool.io/";
-
 const app = {
     data(){
         return {
@@ -9,21 +7,20 @@ const app = {
                 username:"",
                 password:""
             },
+            baseUrl: "https://vue3-course-api.hexschool.io/"
         }
     },
 
     methods:{
         userLogin(){
-            axios.post(`${baseUrl}/v2/admin/signin`,this.user)
+            axios.post(`${this.baseUrl}/v2/admin/signin`,this.user)
                 .then(
                     res => {
                         const {token} = res.data;
                         document.cookie = `hedyToken = ${token}`;
                         window.location = 'products.html';
                     })
-                .catch(
-                    error => alert(error.response.data.message)
-                )
+                .catch(error => alert(error.response.data.message))
         },
     }
 };
