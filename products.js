025dfd31a5aae6a8015c85b3f9533143d4025e85
Hedy <duej123456@gmail.com>
2023-01-15 00:52:21 +0800

    git init

--- products.js
+++ products.js
@@ -1,37 +1,49 @@
 import {createApp} from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
-
+let myModal = new bootstrap.Modal(document.getElementById('productModal'), {
+    keyboard: false
+})
 
 const app = {
     data(){
         return {
-            products:{},
-            temp:[],
             baseUrl: "https://vue3-course-api.hexschool.io",
-            apiPath: "duej123456"
+            apiPath: "duej123456",
+            products:{},
+            tempProduct:{
+                imagesUrl:[],
+            }
         }
     },
 
-    methods: {
+    methods:{
         checkAdmin(){
             axios
                 .post(`${this.baseUrl}/v2/api/user/check`)
-                .then(this.getProducts())
-                .catch((error) => {
-                    alert(error.data.message);
-                    window.location = "login.html"
-                })
+                .then(res => this.getProduct())
+                .catch(err => alert(err))
         },
 
-        getProducts(){
+        getProduct(){
             axios
                 .get(`${this.baseUrl}/v2/api/${this.apiPath}/admin/products/all`)
                 .then(res => this.products = res.data.products)
-                .catch(error => alert(error.response.data.message))
         },
-    },
 
+        createProduct(){
+            myModal.show();
+        },
+
+        postProduct(){
+            console.log("post")
+            axios
+                .post(`${this.baseUrl}/v2/api/${apiPath}/admin/product`)
+                .then(res => console.log(res))
+                .catch(res => console.log(res))
+        }
+    },
+    
     mounted(){
-        const token = document.cookie.replace(/(?:(?:^|.*;\s*)hedyToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+        const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
         axios.defaults.headers.common.Authorization = token;
         this.checkAdmin();
     }
